5245e6fb39a4d151ba634b1c9eda9412eb392aea
Derek <derek.ye@freckle.com>
2023-11-06 16:20:02 -0800

    Finish v0.02

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,14 @@
 module Main (main) where
 
 import Lib
+import Data.Text (Text, pack, unpack)
+import qualified Data.Text.IO as TIO
 
 main :: IO ()
 main = do
-  putStr $ isWordInStringWithKeywords "My favorite color is blue!\n"
-  putStr $ isWordInStringWithKeywords "My favorite color is teal!\n"
+  TIO.putStr $ isWordInStringWithKeywords $ pack "My favorite color is blue!\n" -- should be found
+  TIO.putStr $ isWordInStringWithKeywords $ pack "My favorite color is teal!\n" -- should be not found
+  TIO.putStr $ isWordInStringWithKeywords $ pack "scared\n" -- should be not found
+  TIO.putStr $ isWordInStringWithKeywords $ pack "SpOoKy sCaRy sKeLeToNs\n" -- should be found
+  TIO.putStr $ isWordInStringWithKeywords $ pack "Jack the pumpkinking.\n" -- should be not found
+  TIO.putStr $ isWordInStringWithKeywords $ pack "Carve a pumpkin.\n" -- should be found
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,23 +1,24 @@
 module Lib
-  ( someFunc,
-    isWordInStringWithKeywords,
+  ( isWordInStringWithKeywords,
     isWordInString,
   )
 where
 
 import Data.List
+import Data.Text (Text, empty, pack, split, toLower)
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+splitTextOnCriteria :: Text -> [Text]
+splitTextOnCriteria t = split (\c -> c `elem` [' ', ',', '.', '!', '?']) t
 
-isWordInStringWithKeywords :: String -> String
-isWordInStringWithKeywords = isWordInString ["blue", "green", "yellow", "orange", "red", "purple"]
+isWordInStringWithKeywords :: Text -> Text
+isWordInStringWithKeywords = isWordInString $ map pack ["blue", "green", "yellow", "orange", "red", "purple", "pumpkin", "scary"]
 
-isWordInString :: [String] -> String -> String
-isWordInString [] _ = "No keywords provided"
-isWordInString _ "" = "No keywords found :("
+isWordInString :: [Text] -> Text -> Text
+isWordInString [] _ = pack "No keywords provided"
+isWordInString _ t | t == empty = pack "No keywords found :("
 isWordInString keywords inputStr =
-  let containsWord = any (`isInfixOf` inputStr) keywords
-   in if containsWord
-        then "Keyword found!"
-        else "No keywords found"
+    let splitInput = map toLower $ splitTextOnCriteria inputStr
+        containsWord = any (\word -> word `elem` keywords) splitInput
+    in if containsWord
+        then pack "Keyword found!\n"
+        else pack "No keywords found\n"
