57756fc0d1ab0e2b3a4388e182179cc0c137e7a6
Derek <derek.ye@freckle.com>
2023-11-30 17:17:30 -0800

    Finish lesson 3

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,24 @@ module Main (main) where
 import Lib
 import Data.Text (Text, pack, unpack)
 import qualified Data.Text.IO as TIO
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  TIO.putStr $ isWordInStringWithKeywords $ pack "My favorite color is blue!\n" -- should be found
-  TIO.putStr $ isWordInStringWithKeywords $ pack "My favorite color is teal!\n" -- should be not found
-  TIO.putStr $ isWordInStringWithKeywords $ pack "scared\n" -- should be not found
-  TIO.putStr $ isWordInStringWithKeywords $ pack "SpOoKy sCaRy sKeLeToNs\n" -- should be found
-  TIO.putStr $ isWordInStringWithKeywords $ pack "Jack the pumpkinking.\n" -- should be not found
-  TIO.putStr $ isWordInStringWithKeywords $ pack "Carve a pumpkin.\n" -- should be found
+  -- TIO.putStr $ isWordInStringWithKeywords $ pack "My favorite color is blue!\n" -- should be found
+  -- TIO.putStr $ isWordInStringWithKeywords $ pack "My favorite color is teal!\n" -- should be not found
+  -- TIO.putStr $ isWordInStringWithKeywords $ pack "scared\n" -- should be not found
+  -- TIO.putStr $ isWordInStringWithKeywords $ pack "SpOoKy sCaRy sKeLeToNs\n" -- should be found
+  -- TIO.putStr $ isWordInStringWithKeywords $ pack "Jack the pumpkinking.\n" -- should be not found
+  -- TIO.putStr $ isWordInStringWithKeywords $ pack "Carve a pumpkin.\n" -- should be found
+  -- putStrLn $ show $ splitTextOnCriteria $ pack "My favorite color is teal!"
+  -- putStrLn $ show $ countWordsMapFromList $ splitTextOnCriteria $ pack "My favorite color is teal!"
+  -- putStrLn $ show $ countWordsMapFromList $ splitTextOnCriteria $ pack "orange orange blue aqua blue green red teal"
+  args <- getArgs
+  case args of 
+    [keywordStr] -> do
+      printAllWordcounts $ wordcountMapFromList $ normalizeInputStr keywordStr
+      printFoundKeywordCounts $ wordcountMapFromList $ normalizeInputStr keywordStr
+      printNonWordcounts $ wordcountMapFromList $ normalizeInputStr keywordStr 
+    _ -> do
+      putStrLn "Please provide a string with all the keywords"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,24 +1,84 @@
 module Lib
-  ( isWordInStringWithKeywords,
-    isWordInString,
+  -- ( isWordInStringWithKeywords,
+  --   isWordInString,
+  (  normalizeInputStr,
+    wordcountMapFromList,
+    splitTextOnCriteria,
+    printAllWordcounts,
+    printFoundKeywordCounts,
+    filterKeywords,
+    printNonWordcounts
   )
 where
 
-import Data.List
+import qualified Data.Map as M
 import Data.Text (Text, empty, pack, split, toLower)
+import Text.Printf
+import Data.List
+import qualified Data.Text as T
+import Data.Maybe (fromMaybe)
+
+-- hard-coded keywords to search for
+keywords :: [Text]
+keywords = map pack ["blue", "green", "yellow", "orange", "red", "purple"]
+
+wordcountMapFromList :: [Text] -> M.Map Text Int
+wordcountMapFromList wordlist =  wordcountMapFromList' wordlist M.empty
+
+wordcountMapFromList' :: [Text] -> M.Map Text Int -> M.Map Text Int
+wordcountMapFromList' [] accMap = accMap
+wordcountMapFromList' (currWord : xs) accMap
+  -- {'apple': 1}
+  -- if map contain word increment count
+  | M.member currWord accMap = wordcountMapFromList' xs (M.insert currWord newCount accMap)
+  -- otherwise set count to 1
+  | otherwise = wordcountMapFromList' xs (M.insert currWord 1 accMap)
+  where 
+    oldCount = fromMaybe 0 (M.lookup currWord accMap) -- Extract value or default to 0 if key not found
+    newCount = oldCount + 1
 
 splitTextOnCriteria :: Text -> [Text]
-splitTextOnCriteria t = split (\c -> c `elem` [' ', ',', '.', '!', '?']) t
+splitTextOnCriteria t = filter (not . T.null) $ split (\c -> c `elem` [' ', ',', '.', '!', '?']) t
 
-isWordInStringWithKeywords :: Text -> Text
-isWordInStringWithKeywords = isWordInString $ map pack ["blue", "green", "yellow", "orange", "red", "purple", "pumpkin", "scary"]
+-- split text, strip symbols
+normalizeInputStr :: String -> [Text]
+normalizeInputStr inputStr = map toLower $ splitTextOnCriteria $ pack inputStr
 
-isWordInString :: [Text] -> Text -> Text
-isWordInString [] _ = pack "No keywords provided"
-isWordInString _ t | t == empty = pack "No keywords found :("
-isWordInString keywords inputStr =
-    let splitInput = map toLower $ splitTextOnCriteria inputStr
-        containsWord = any (\word -> word `elem` keywords) splitInput
-    in if containsWord
-        then pack "Keyword found!\n"
-        else pack "No keywords found\n"
+-- printing functions
+--  print in the format
+--  Found keywords:
+--  purple: 1
+--  green: 2
+--  ...
+printFoundKeywordCounts :: M.Map Text Int -> IO ()
+printFoundKeywordCounts m = putStr formattedKwCountStr 
+  where
+    keywordCountMap = filterKeywords m
+    keywordCountArr = map formatWordCount $ M.toList keywordCountMap
+    formattedKwCountStr = "Found keywords:\n" ++ intercalate "" keywordCountArr   ++ "\n"
+
+printAllWordcounts :: M.Map Text Int -> IO ()
+printAllWordcounts m = putStr formattedKwCountStr 
+  where
+    wordCountArr = map formatWordCount $ M.toList m
+    formattedKwCountStr = "All word counts:\n" ++ intercalate "" wordCountArr ++ "\n"
+
+printNonWordcounts :: M.Map Text Int -> IO ()
+printNonWordcounts m = putStr formattedKwCountStr 
+  where
+    nonKeywordMap = filterNonKeywords m
+    formattedKwCountStr = "Non-keyword count:" ++ show (M.size nonKeywordMap) ++ "\n"
+
+-- function to format individual word, count values
+formatWordCount :: (Show a, Show b) => (a, b) -> String
+formatWordCount (k, v) = printf "%s: %s\n" (show k) (show v)
+
+-- util functions
+filterKeywords :: M.Map Text Int -> M.Map Text Int
+filterKeywords = M.filterWithKey (\key _ -> isKeyword key)
+
+filterNonKeywords :: M.Map Text Int -> M.Map Text Int
+filterNonKeywords = M.filterWithKey (\key _ -> not $ isKeyword key) 
+    
+isKeyword :: Text -> Bool
+isKeyword word = word `elem` keywords
